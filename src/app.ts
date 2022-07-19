ae0d840a8add093ed4edde479f8e988fa9b93f18
Magno batista <magnobatista75@gmail.com>
2022-07-19 00:30:24 -0300

    desafio 03

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,40 @@
-console.log('Arquivo de testes. Pode mexer nele como quiser.')
-
-let employee: { code: number, name: string }
-
-employee = { code: 10, name: "desafio 01" }
 
 
-console.log("ver saida", employee)
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
 
-////
-// desafio 02
+let soma = document.getElementById('soma') as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo') as HTMLInputElement;
 
+campoSaldo.innerHTML = '0' // para apresentar na tela
+let resultadoSoma: number = 0;
 
-class pessoa {
-    private nome: string;
-    private idade: number;
-    private profissao: string;
+function somarAoSaldo(numero?: number | string) {
 
+    console.log("typeof ", typeof numero);
 
-    constructor(nome: string, idade: number, profissao: string) {
-        this.nome = nome;
-        this.idade = idade;
-        this.profissao = profissao;
+    if (typeof numero === "number") {
+        resultadoSoma += numero;
+    } else {
+        resultadoSoma = resultadoSoma + Number(numero);
     }
 
+    campoSaldo.innerHTML = "RS " + resultadoSoma.toFixed(2).toString();
 }
 
-let pessoa1 = new pessoa('maria', 29, 'atriz');
-let pessoa2 = new pessoa('roberto', 19, 'padeiro');
-
-console.log("desafio 02 ultilizando class ", pessoa1, pessoa2);
-/* let pessoa1 = {};
-pessoa1.nome = "maria";
-pessoa1.idade = 29;
-pessoa1.profissao = "atriz" */
-
-/* let pessoa2 = {}
-pessoa2.nome = "roberto";
-pessoa2.idade = 19;
-pessoa2.profissao = "Padeiro"; */
-
-let listPessoas: Array<pessoa> = [];
-
-
-let pessoa3 = new pessoa("laura", 32, "Atriz");
-let pessoa4 = new pessoa("carlos", 19, "padeiro")
-
-
-listPessoas.push(pessoa1);
-listPessoas.push(pessoa2);
-listPessoas.push(pessoa3);
-listPessoas.push(pessoa4);
-
-
-for (const iterator of listPessoas) {
-    console.log('mostrar de pessoas ultilizando for of', iterator)
+function limparSaldo() {
+    campoSaldo.innerHTML = '';
+    resultadoSoma = 0;
 }
+
+botaoAtualizar?.addEventListener('click', function () {
+
+    somarAoSaldo(soma.value);
+
+});
+
+botaoLimpar?.addEventListener('click', function () {
+    limparSaldo();
+});
+
 

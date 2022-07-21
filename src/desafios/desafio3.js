40f0c33ac5859aafba1f7f528a2f05c062149445
Magno batista <magnobatista75@gmail.com>
2022-07-21 00:01:44 -0300

    adicionado metodo validarhtmlinputelement

--- src/desafios/desafio3.js
+++ src/desafios/desafio3.js
@@ -29,3 +29,49 @@ botaoLimpar.addEventListener('click', function () {
     <button id="limpar-saldo">Limpar seu saldo</button>
     <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
  */
+
+
+
+/*
+ 
+ 
+
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+
+let soma = document.getElementById('soma') as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo') as HTMLInputElement;
+
+campoSaldo.innerHTML = '0' // para apresentar na tela
+let resultadoSoma: number = 0;
+
+function somarAoSaldo(numero?: number | string) {
+
+console.log("typeof ", typeof numero);
+
+if (typeof numero === "number") {
+    resultadoSoma += numero;
+} else {
+    resultadoSoma = resultadoSoma + Number(numero);
+}
+
+campoSaldo.innerHTML = "RS " + resultadoSoma.toFixed(2).toString();
+}
+
+function limparSaldo() {
+campoSaldo.innerHTML = '';
+resultadoSoma = 0;
+}
+
+botaoAtualizar?.addEventListener('click', function () {
+
+somarAoSaldo(soma.value);
+
+});
+
+botaoLimpar?.addEventListener('click', function () {
+limparSaldo();
+});
+
+ 
+*/
